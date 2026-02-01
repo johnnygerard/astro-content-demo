8c9972fcfded8531e019bc80880694f3aa4f79e8
Johnny Gérard <john@jgerard.dev>
2026-02-01 04:05:58 +0100

    feat: define blog collection

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "*.md", base: "src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string().optional(),
+    publicationDate: z.string().datetime(),
+    tags: z.array(z.string()).optional(),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+  }),
+});
+
+export const collections = { blog };
